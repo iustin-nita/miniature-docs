f0f85d047c8f2cd501aacbe2b752d2d27944341b
iustinn <iustin.nitza@gmail.com>
2016-02-08 10:41:54 +0200

    add method to keep track of the editing users

--- miniature-docs.js
+++ miniature-docs.js
@@ -1,5 +1,5 @@
 this.Documents = new Mongo.Collection("documents");
-
+EditingUsers = new Mongo.Collection("editingUsers");
 if (Meteor.isClient) {
 
   Template.editor.helpers({
@@ -25,9 +25,12 @@ if (Meteor.isClient) {
 
     config: function() {
       return function(editor) {
+        editor.setOption("lineNumbers", true);
+        editor.setOption("mode","html");
         editor.on("change", function(cm_editor, info) {
           console.log(cm_editor.getValue());
           $("#preview_iframe").contents().find("html").html(cm_editor.getValue());
+          Meteor.call("addEditingUser");
         });
       };
     }
@@ -49,3 +52,27 @@ if (Meteor.isServer) {
     }
   });
 }
+
+
+Meteor.methods({
+  addEditingUser: function() {
+    var doc, user, eusers;
+    doc = Documents.findOne();
+    if (!doc) {return;} //no doc give up
+
+    if(!this.userId){return;} //no logged in user give up
+
+    // now I have a doc and possibly a user
+    user = Meteor.user().profile;
+    eusers = EditingUsers.findOne({docid: doc._id});
+    if (!eusers) {
+      eusers = {
+        docid: doc._id,
+        users: {}
+      };
+    }
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+    EditingUsers.upsert({_id: eusers.id},{user: 'sdasd'});
+  }
+});
